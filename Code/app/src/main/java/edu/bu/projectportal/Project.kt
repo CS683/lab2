db7d292f068aeca440c609d6ab57509c042e8f5a
Shanghua Yang <ysh99226@gmail.com>
2023-10-26 13:24:09 -0400

    finished

---
+++ Code/app/src/main/java/edu/bu/projectportal/Project.kt
@@ -0,0 +1,21 @@
+package edu.bu.projectportal
+
+data class Project(val id: Int,
+                   var title: String,
+                   var description: String,
+                   var authors: List<String> = listOf("Shanghua Yang"),
+                   var link: String = "",
+                   var isFavorite: Boolean = false,
+                   var keywords: Set<String> = setOf()
+   ){
+    companion object {
+        val project = Project(0, "Weather Forecast", "Weather Forcast is an app ...")
+    }
+
+//        val projects = listOf(
+//            Project(0, "Weather Forecast", "Weather Forcast is an app ..."),
+//            Project(1, "Connect Me", "Connect Me is an app ... "),
+//            Project(2, "What to Eat", "What to Eat is an app ..."),
+//            Project(3, "Project Portal", "Project Portal is an app ..."))
+//    }
+}
